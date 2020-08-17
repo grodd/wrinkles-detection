99fb5bd277aede39a1cd87d319ea172ee02e978c
grodd <52341961+grodd@users.noreply.github.com>
2020-08-17 05:30:21 +0530

    Optimised the code

--- app.py
+++ app.py
@@ -14,16 +14,6 @@ from werkzeug.utils import secure_filename
 warnings.filterwarnings("ignore")
 torch.nn.Module.dump_patches = True
 
-def getI420FromBase64(codec, image_path="./uploads/"):
-    base64_data = re.sub('^data:image/.+;base64,', '', codec)
-    byte_data = base64.b64decode(base64_data)
-    image_data = BytesIO(byte_data)
-    img = Image.open(image_data)
-    t = time.time()
-    # img.save(os.path.join(uploads_dir, secure_filename(str(t) + '.png')), "PNG")
-    img.save(image_path + str(t) + '.png', 'PNG')
-    return str(t)
-
 app = Flask(__name__)
 CORS(app)
 
@@ -31,22 +21,20 @@ CORS(app)
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 os.makedirs(uploads_dir, exist_ok=True)
 
-learn = load_learner(os.path.dirname(os.path.abspath(__file__)+'model_1'), 'export.pkl')
+learn = load_learner('./model_1')
 learn_2 = load_learner('./model_2/')
 learn_3 = load_learner('./model_3/')
 
 @app.route("/", methods=['GET','POST'])
 def home():
     if request.method == 'POST':
-        data = request.json['image']
-        return output(data)
+        return output(request.files['image'].read())
     else:
         return render_template('index.html')
 
 
 def output(data):
-  name = getI420FromBase64(data)
-  img = open_image('./uploads/' + name + '.png')
+  img = open_image(BytesIO(data))
   img.resize(torch.Size([img.shape[0], 500, 500]))
   pred_1 = learn.predict(img)[1]
   mask_1 = pred_1.numpy()[0]
@@ -54,7 +42,8 @@ def output(data):
   mask_2 = pred_2.numpy()[0]
   pred_3 = learn_3.predict(img)[1]
   mask_3 = pred_3.numpy()[0]
-  img_cv = cv2.imread('./uploads/'+ name + '.png')
+  image = np.asarray(bytearray(data), dtype="uint8")
+  img_cv = cv2.imdecode(image, cv2.IMREAD_COLOR)
   img_cv = cv2.resize(img_cv,(500, 500))
   mask_dark_circles = np.copy(mask_1)
   mask_dark_circles[mask_dark_circles==3] = 2
@@ -64,8 +53,9 @@ def output(data):
   img_cv[mask_1 == 4] = 150
   img_cv[mask_2 == 5] = 0
   img_cv[mask_3== 6] = 255
-  cv2.imwrite('./output/' + name + '.png', img_cv)
-  filename = './output/'+name+'.png'
+  t = time.time()
+  cv2.imwrite('./output/' + str(t) + '.png', img_cv)
+  filename = './output/'+str(t)+'.png'
   with open(filename, "rb") as img_file:
     my_string = base64.b64encode(img_file.read()).decode('ascii')
   # return send_file(filename, mimetype='image/gif')
